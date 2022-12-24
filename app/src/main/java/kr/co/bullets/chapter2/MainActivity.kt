40ed85f6861f078588427ad13941e643f22849d5
Bullets-jyr <jyr@bullets.co.kr>
2022-12-24 14:01:10 +0900

    05. 유저 입력값 받아오기

--- app/src/main/java/kr/co/bullets/chapter2/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/chapter2/MainActivity.kt
@@ -2,10 +2,25 @@ package kr.co.bullets.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+        resetButton.setOnClickListener {
+            Log.d("onClick", "리셋 버튼이 클릭 됐습니다.")
+        }
+
+        plusButton.setOnClickListener {
+            Log.e("onClick", "플러스 버튼이 클릭 됐습니다.")
+        }
     }
 }
